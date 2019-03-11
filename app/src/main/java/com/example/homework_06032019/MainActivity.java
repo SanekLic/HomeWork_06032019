abd9d7e8e157ecd9c27821d1605e1a79b58e8c23
Saneklic <saneklic@gmail.com>
2019-03-11 22:00:28 +0300

    added toolbar

--- app/src/main/java/com/example/homework_06032019/MainActivity.java
+++ app/src/main/java/com/example/homework_06032019/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.homework_06032019;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+        mainToolbar.inflateMenu(R.menu.menu_toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+
+        return true;
     }
 }

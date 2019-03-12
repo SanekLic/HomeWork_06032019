e3d0f6c22e51f1f20718d6864415924019f61e9a
sanek <saneklic@gmail.com>
2019-03-12 17:55:34 +0300

    added user name, online state, change button, status, count friends, home

--- app/src/main/java/com/example/homework_06032019/MainVkActivity.java
+++ app/src/main/java/com/example/homework_06032019/MainVkActivity.java
@@ -5,18 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
-    private Toolbar mainToolbar;
+public class MainVkActivity extends AppCompatActivity {
+    private Toolbar mainVkToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.activity_main_vk);
 
-        mainToolbar = findViewById(R.id.mainToolbar);
-        setSupportActionBar(mainToolbar);
-        mainToolbar.inflateMenu(R.menu.menu_toolbar);
+        mainVkToolbar = findViewById(R.id.mainVkToolbar);
+        setSupportActionBar(mainVkToolbar);
+        mainVkToolbar.inflateMenu(R.menu.menu_vk_toolbar);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_toolbar, menu);
+        inflater.inflate(R.menu.menu_vk_toolbar, menu);
 
         return true;
     }

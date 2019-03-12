4679dc8a199ca1dd262ed4156ec2d8936dd79fe1
Saneklic <saneklic@gmail.com>
2019-03-12 22:51:57 +0300

    added google podcasts activity

---
+++ app/src/main/java/com/example/homework_06032019/MainGooglePodcastsActivity.java
@@ -0,0 +1,36 @@
+package com.example.homework_06032019;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
+
+public class MainGooglePodcastsActivity extends AppCompatActivity {
+    private Toolbar mainGoolgePodcastsToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.activity_main_google_podcasts);
+
+        mainGoolgePodcastsToolbar = findViewById(R.id.mainGoolgePodcastsToolbar);
+        setSupportActionBar(mainGoolgePodcastsToolbar);
+        mainGoolgePodcastsToolbar.inflateMenu(R.menu.menu_google_podcasts_toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_google_podcasts_toolbar, menu);
+
+        return true;
+    }
+}

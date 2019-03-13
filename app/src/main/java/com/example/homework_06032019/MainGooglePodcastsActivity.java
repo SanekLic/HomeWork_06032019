094a01f9224da2102bdcc2e8476baa00ab016345
Saneklic <saneklic@gmail.com>
2019-03-13 21:02:01 +0300

    complete google podcasts

--- app/src/main/java/com/example/homework_06032019/MainGooglePodcastsActivity.java
+++ app/src/main/java/com/example/homework_06032019/MainGooglePodcastsActivity.java
@@ -1,6 +1,8 @@
 package com.example.homework_06032019;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -9,6 +11,8 @@ import android.view.Window;
 
 public class MainGooglePodcastsActivity extends AppCompatActivity {
     private Toolbar mainGoolgePodcastsToolbar;
+    private TabLayout forYouTabLayout;
+    private ViewPager forYouViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,32 @@ public class MainGooglePodcastsActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_main_google_podcasts);
 
-        mainGoolgePodcastsToolbar = findViewById(R.id.mainGoolgePodcastsToolbar);
+        initialView();
+        setToolbar();
+        setTabLayout();
+    }
+
+    private void setTabLayout() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new NewEpisodesFragment(), "New episodes");
+        adapter.addFragment(new InProgressFragment(), "In progress");
+        adapter.addFragment(new DownloadsFragment(), "Downloads");
+        forYouViewPager.setAdapter(adapter);
+        forYouTabLayout.setupWithViewPager(forYouViewPager);
+    }
+
+    private void setToolbar() {
         setSupportActionBar(mainGoolgePodcastsToolbar);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+    }
+
+    private void initialView() {
+        mainGoolgePodcastsToolbar = findViewById(R.id.mainGoolgePodcastsToolbar);
+        forYouTabLayout = findViewById(R.id.forYouTabLayout);
+        forYouViewPager = findViewById(R.id.forYouViewPager);
     }
 
     @Override
